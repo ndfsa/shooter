4a2d2bccf9ae5e5d254e8adfca0d2d7471330b9d
ndfsa <sergiomarzana1@gmail.com>
2018-11-05 18:25:40 -0400

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Target = new System.Windows.Forms.PictureBox();
+            this.Cannon = new System.Windows.Forms.PictureBox();
+            this.bulletTime = new System.Windows.Forms.Timer(this.components);
+            this.Projectile = new System.Windows.Forms.PictureBox();
+            this.targetTime = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Target)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cannon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Projectile)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Target
+            // 
+            this.Target.BackColor = System.Drawing.Color.DarkRed;
+            this.Target.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Target.Location = new System.Drawing.Point(725, 507);
+            this.Target.Name = "Target";
+            this.Target.Size = new System.Drawing.Size(39, 37);
+            this.Target.TabIndex = 0;
+            this.Target.TabStop = false;
+            // 
+            // Cannon
+            // 
+            this.Cannon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Cannon.Location = new System.Drawing.Point(42, 29);
+            this.Cannon.Name = "Cannon";
+            this.Cannon.Size = new System.Drawing.Size(50, 50);
+            this.Cannon.TabIndex = 1;
+            this.Cannon.TabStop = false;
+            // 
+            // bulletTime
+            // 
+            this.bulletTime.Enabled = true;
+            this.bulletTime.Interval = 10;
+            this.bulletTime.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Projectile
+            // 
+            this.Projectile.BackColor = System.Drawing.Color.Yellow;
+            this.Projectile.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Projectile.Location = new System.Drawing.Point(59, 44);
+            this.Projectile.Name = "Projectile";
+            this.Projectile.Size = new System.Drawing.Size(14, 16);
+            this.Projectile.TabIndex = 2;
+            this.Projectile.TabStop = false;
+            // 
+            // targetTime
+            // 
+            this.targetTime.Enabled = true;
+            this.targetTime.Interval = 10;
+            this.targetTime.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 853);
+            this.Controls.Add(this.Projectile);
+            this.Controls.Add(this.Cannon);
+            this.Controls.Add(this.Target);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Target)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cannon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Projectile)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Target;
+        private System.Windows.Forms.PictureBox Cannon;
+        private System.Windows.Forms.Timer bulletTime;
+        private System.Windows.Forms.PictureBox Projectile;
+        private System.Windows.Forms.Timer targetTime;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        private bool t_up;
+        private bool t_down;
+        private bool t_left;
+        private bool t_right;
+        private bool c_up;
+        private bool c_down;
+        private bool c_left;
+        private bool c_right;
+        private double bulletSpeed = 20;
+        private double targetSpeed = 8;
+        private double cannonSpeed = 3;
+        private double p_x;
+        private double p_y;
+        private double t_x;
+        private double t_y;
+
+        //private int projectileInitialPosX;
+        //private int projectileInitialPosY;
+        private bool projFired = false;
+        private double angle = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    t_up = true;
+                    break;
+                case Keys.Down:
+                    t_down = true;
+                    break;
+                case Keys.Left:
+                    t_left = true;
+                    break;
+                case Keys.Right:
+                    t_right = true;
+                    break;
+            }
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    c_up = true;
+                    break;
+                case Keys.S:
+                    c_down = true;
+                    break;
+                case Keys.A:
+                    c_left = true;
+                    break;
+                case Keys.D:
+                    c_right = true;
+                    break;
+            }
+
+            targetTime.Start();
+
+            //Invalidate();
+        }
+        private void _DoMovement()
+        {
+            if (t_left) Target.Left -= (int)targetSpeed;
+            if (t_right) Target.Left += (int)targetSpeed;
+            if (t_up) Target.Top -= (int)targetSpeed;
+            if (t_down) Target.Top += (int)targetSpeed;
+
+            if (c_left) Cannon.Left -= (int)cannonSpeed;
+            if (c_right) Cannon.Left += (int)cannonSpeed;
+            if (c_up) Cannon.Top -= (int)cannonSpeed;
+            if (c_down) Cannon.Top += (int)cannonSpeed;
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            if (projFired)
+            {
+                projMove();
+            }
+            else
+            {
+                shoot();
+            }
+            if (Projectile.Bounds.IntersectsWith(Target.Bounds))
+            {
+                Console.WriteLine("You lost");
+                //Application.Exit();
+            }
+        }
+
+        private void projMove()
+        {
+            Projectile.Top += (int)(Math.Sin(angle) * bulletSpeed);
+            Projectile.Left += (int)(Math.Cos(angle) * bulletSpeed);
+            //Target.Top -= targetVelocity;
+            //Target.Left -= targetVelocity;
+            if (Projectile.Top < -4 || Projectile.Left > 655 || Projectile.
+                Left < -4 || Projectile.Top > 685)
+            {
+                projFired = false;
+                Projectile.Top = Cannon.Top + (Cannon.Height / 2);
+                Projectile.Left = Cannon.Left + (Cannon.Width / 2);
+            }
+            Invalidate();
+        }
+
+        private void shoot()
+        {
+            calculateAngle();
+            projFired = true;
+        }
+
+        private void calculateAngle()
+        {
+            t_x = Target.Left + (Target.Width / 2);
+            t_y = Target.Top + (Target.Height / 2);
+
+            p_x = Projectile.Left + (Projectile.Width / 2);
+            p_y = Projectile.Top + (Projectile.Height / 2);
+            double deltaX = t_x - p_x;
+            double deltaY = t_y - p_y;
+            double alpha0 = Math.Atan2(deltaY , deltaX);
+
+            if (!(t_down || t_left || t_right || t_up) || (t_left && t_right && !t_up && !t_down) || (!t_left && !t_right && t_up && t_down)||(t_left && t_right && t_up && t_down))
+            //if (false)
+            {
+                this.angle = alpha0;
+            }
+            else
+            {
+                //double aux = (getTargetSpeed()/ bulletSpeed) * Math.Sin(alpha0 + getTargetDirection());
+                this.angle = Math.Asin(getTargetSpeed() / bulletSpeed) * Math.Sin(alpha0 + getTargetDirection()) + alpha0;
+                //Console.WriteLine("the auxiliary" + aux);
+            }
+            Console.WriteLine(this.angle);
+            //this.angle += 0.3;
+
+        }
+
+        private double getTargetDirection()
+        {
+            double targetAngle = 0;
+            if ((t_left && !t_right && !t_up && !t_down)||(t_left && !t_right && t_up && t_down)) targetAngle = 0;
+            else if ((!t_left && !t_right && !t_up && t_down)||(t_left && t_right && !t_up && t_down)) targetAngle = Math.PI / 2;
+            else if ((!t_left && t_right && !t_up && !t_down)||(!t_left && t_right && t_up && t_down)) targetAngle = Math.PI;
+            else if ((!t_left && !t_right && t_up && !t_down)||(t_left && t_right && t_up && !t_down)) targetAngle = 3 * Math.PI / 2;
+            else if (t_left && !t_right && !t_up && t_down) targetAngle = Math.PI / 4;
+            else if (!t_left && t_right && !t_up && t_down) targetAngle = 3 * Math.PI / 4;
+            else if (!t_left && t_right && t_up && !t_down) targetAngle = 5 *Math.PI /4;
+            else if (t_left && !t_right && t_up && !t_down) targetAngle = 7*Math.PI /4;
+
+            return targetAngle;
+        }
+
+        private double getTargetSpeed()
+        {
+            return (onlyOne(t_up, t_down, t_left, t_right) ? targetSpeed : targetSpeed * Math.Sqrt(2));
+        }
+        private bool onlyOne(bool a, bool b, bool c, bool d)
+        {
+            return (a && !b && !c && !d) || (!a && b && !c && !d)|| (!a && !b && c && !d)|| (!a && !b && !c && d);
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            _DoMovement();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    t_up = false;
+                    break;
+                case Keys.Down:
+                    t_down = false;
+                    break;
+                case Keys.Left:
+                    t_left = false;
+                    break;
+                case Keys.Right:
+                    t_right = false;
+                    break;
+            }
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    c_up = false;
+                    break;
+                case Keys.S:
+                    c_down = false;
+                    break;
+                case Keys.A:
+                    c_left = false;
+                    break;
+                case Keys.D:
+                    c_right = false;
+                    break;
+            }
+            if (!(t_down || t_left || t_right || t_up) && !(c_down || c_left || c_right || c_up))
+            {
+                targetTime.Stop();
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.Black, (float)t_x - 1, (float)t_y - 1, 1, 1);
+        }
+    }
+}
